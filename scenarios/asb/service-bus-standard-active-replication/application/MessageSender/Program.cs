15e2dd19fc7ff1500924286e8bf6f77556683237
Andrew Twigg <andrew.twigg@outlook.com>
2022-01-16 12:03:52 +0000

    sb std active active

--- scenarios/asb/service-bus-standard-active-replication/application/MessageSender/Program.cs
+++ scenarios/asb/service-bus-standard-active-replication/application/MessageSender/Program.cs
@@ -14,13 +14,62 @@ namespace MessagingSender
         {
             Console.WriteLine($"Messaging Sender started. Primary bus: {sbPrimary}, Secondary bus: {sbSecondary}");
 
-            const string queueName = "MyQueue";
+            const string queueName = "myqueue";
 
-            string fullyQualifiedNamespacePri = $"{sbPrimary}.servicebus.windows.net";
-            await using var client = new ServiceBusClient(fullyQualifiedNamespacePri, new DefaultAzureCredential());
+            await using var clientPri = new ServiceBusClient($"{sbPrimary}.servicebus.windows.net", new DefaultAzureCredential());
+            await using var clientSec = new ServiceBusClient($"{sbSecondary}.servicebus.windows.net", new DefaultAzureCredential());
 
-            //// Create the sender
-            //ServiceBusSender sender = client.CreateSender(queueName);
+            // Create the sender
+            ServiceBusSender senderPri = clientPri.CreateSender(queueName);
+            ServiceBusSender senderSec = clientSec.CreateSender(queueName);
+
+            Console.WriteLine("\nSending messages to primary and secondary queues...\n");
+
+            // TODO: Error handling?
+
+            for (int i = 0; i < 5; i++)
+            {
+                var m1 = new ServiceBusMessage("Message" + i)
+                {
+                    MessageId = i.ToString(),
+                    TimeToLive = TimeSpan.FromMinutes(2.0)
+                };
+
+                var m2 = new ServiceBusMessage("Message" + i)
+                {
+                    MessageId = i.ToString(),
+                    TimeToLive = TimeSpan.FromMinutes(2.0)
+                };
+
+                var exceptionCount = 0;
+
+                var t1 = senderPri.SendMessageAsync(m1);
+                var t2 = senderSec.SendMessageAsync(m2);
+
+                // collect result for primary queue
+                try
+                {
+                    await t1;
+                    Console.WriteLine("Message {0} sent to primary queue: Body = {1}", m1.MessageId, m1.Body.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unable to send message {0} to primary queue: Exception {1}", m1.MessageId, e);
+                    exceptionCount++;
+                }
+
+                // collect result for secondary queue
+                try
+                {
+                    await t2;
+                    Console.WriteLine("Message {0} sent to secondary queue: Body = {1}", m2.MessageId, m2.Body.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unable to send message {0} to secondary queue: Exception {1}", m2.MessageId, e);
+                    exceptionCount++;
+                }
+            }
 
             //while (true)
             //{
