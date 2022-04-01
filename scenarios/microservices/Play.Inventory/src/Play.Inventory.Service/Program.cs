9906adb912b7b123acf637335c46f7367c1fb90c
Andrew Twigg <andrew.twigg@outlook.com>
2022-04-01 08:41:14 +0100

    microservices

---
+++ scenarios/microservices/Play.Inventory/src/Play.Inventory.Service/Program.cs
@@ -0,0 +1,34 @@
+using Play.Common.MongoDB;
+using Play.Common.Settings;
+using Play.Inventory.Service.Entities;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+ServiceSettings serviceSettings = builder.Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+
+builder.Services.AddMongo()
+                .AddMongoRepository<InventoryItem>("inventoryitems");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
