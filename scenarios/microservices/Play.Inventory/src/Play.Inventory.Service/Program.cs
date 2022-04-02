5b03ec54be86bbb15389f2a6148d2dfe32e04735
Andrew Twigg <andrew.twigg@outlook.com>
2022-04-02 10:37:00 +0100

    microservices

--- scenarios/microservices/Play.Inventory/src/Play.Inventory.Service/Program.cs
+++ scenarios/microservices/Play.Inventory/src/Play.Inventory.Service/Program.cs
@@ -2,6 +2,8 @@ using Play.Common.MongoDB;
 using Play.Common.Settings;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
+using Polly;
+using Polly.Timeout;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,7 +19,24 @@ ServiceSettings serviceSettings = builder.Configuration.GetSection(nameof(Servic
 builder.Services.AddMongo()
                 .AddMongoRepository<InventoryItem>("inventoryitems");
 
+Random jitterer = new();
+
 builder.Services.AddHttpClient<CatalogClient>(client => client.BaseAddress = new Uri("https://localhost:7242"))
+    .AddTransientHttpErrorPolicy(b => b.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+        retryCount: 5,
+        // Backoff with randomness
+        retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000)),
+        // Don't do this in production code. It's here to monitor the retry behavior.
+        // Warning ASP0000: Calling 'BuildServiceProvider' from application code results in an additional copy
+        // of singleton services being created.
+        onRetry: (outcome, timespan, retryAttempt) =>
+        {
+            var servcieProvider = builder.Services.BuildServiceProvider();
+            servcieProvider.GetService<ILogger<CatalogClient>>()?
+                .LogWarning($"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryAttempt}");
+        }
+    ))
+    .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1))
     .ConfigurePrimaryHttpMessageHandler(() =>
         new HttpClientHandler
         {
